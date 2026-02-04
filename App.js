85e98f382b8f6d5899d108f52761ae2f816eb125
Fares Boubaker <faresboubaker07@gmail.com>
2026-02-04 22:06:05 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>مرحبًا بك في تطبيق NAJDA!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
+  text: { fontSize: 22, color: '#333', textAlign: 'center' }
+});
